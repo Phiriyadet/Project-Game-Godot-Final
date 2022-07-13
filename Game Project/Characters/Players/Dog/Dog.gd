c1b913ae2621ca811e6daf62c30305ca813f8211
Phiriyadet <Phiriyadech@gmail.com>
2022-07-13 16:46:59 +0700

    change picture dog

--- Game Project/Characters/Players/Dog/Dog.gd
+++ Game Project/Characters/Players/Dog/Dog.gd
@@ -1,12 +1,5 @@
 extends Character
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
@@ -17,10 +10,10 @@ func get_input() -> void:
 		mov_direction += Vector2.DOWN
 	if Input.is_action_pressed("ui_left"):
 		mov_direction += Vector2.LEFT
-		animated_sprite.flip_h  = false
+		animated_sprite.flip_h  = true
 	if Input.is_action_pressed("ui_right"):
 		mov_direction += Vector2.RIGHT
-		animated_sprite.flip_h = true
+		animated_sprite.flip_h  = false
 	if Input.is_action_pressed("ui_up"):
 		mov_direction += Vector2.UP
 		
