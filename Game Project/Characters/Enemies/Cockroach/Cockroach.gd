ac4ca6eeee0cd85da32d213604cfec5b4e90678f
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2022-11-30 16:28:40 +0700

    เปลี่ยน ภาพ ของ uganda knuckle

---
+++ Game Project/Characters/Enemies/Cockroach/Cockroach.gd
@@ -0,0 +1,15 @@
+extends "res://Characters/Enemies/Enemy.gd"
+
+class_name Cockroach, "res://Assets/Characters/Enemies/Cockroach_dancing/cockroach-1.png"
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(_delta: float):
+	hitbox.knockback_direction = velocity.normalized()
