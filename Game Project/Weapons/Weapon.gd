0fff3aab3785aa67b1c076c0020976aba19aefdc
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2022-12-09 18:00:11 +0700

    ตัวละครใหม่และปรับปรุงอาวุธ

--- Game Project/Weapons/Weapon.gd
+++ Game Project/Weapons/Weapon.gd
@@ -2,17 +2,18 @@ extends Node2D
 class_name Weapon
 
 export(int) var atk_w = 0 setget set_atk_w, get_atk_w
+export(int) var level_Weapon = 1
 onready var animated_sprite : = get_node("Node2D/Sprite")
 onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
 onready var hitbox: Area2D = get_node("Node2D/Sprite/Hitbox")
-onready var cool_down_timer: Timer = get_node("CoolDownTimer")
+onready var cooldown_timer: Timer = get_node("CoolDownTimer")
 
 
 func _init():
 	pass
 	
 func _ready():
-	cool_down_timer.start()
+	cooldown_timer.start()
 	
 func attack():
 	animation_player.play("attack")
