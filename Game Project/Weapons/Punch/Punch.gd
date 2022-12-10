32607cf157583ad52103a1e6183b0acff5bed164
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2022-12-10 14:28:13 +0700

    new weapon

--- Game Project/Weapons/Punch/Punch.gd
+++ Game Project/Weapons/Punch/Punch.gd
@@ -4,7 +4,7 @@ extends "res://Weapons/Weapon.gd"
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-class_name Punch
+class_name Punch,"res://Assets/Weapons/punch/punch.png"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
