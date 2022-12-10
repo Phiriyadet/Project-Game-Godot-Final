1604ed13e6455810bca64c73c14929086877a11f
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2022-12-10 13:26:28 +0700

    test2

--- Game Project/Characters/Players/PepeTheFrog/PepeTheFrog.gd
+++ Game Project/Characters/Players/PepeTheFrog/PepeTheFrog.gd
@@ -1,10 +1,10 @@
 extends "res://Characters/Players/Player.gd"
 
-class_name PepeTheFrog, "res://Assets/Characters/Players/Pepe/pepe-idle-1.png"
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+class_name PepeTheFrog , "res://Assets/Characters/Players/Pepe/pepe-idle-1.png"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
