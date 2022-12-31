6e291aeae2d8ab997179de80eba97dd38ddd9349
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2022-12-31 17:16:06 +0700

    new weapon nokia3310

---
+++ Game Project/Weapons/Nokia3310/Nokia3310.gd
@@ -0,0 +1,16 @@
+extends "res://Weapons/Weapon.gd"
+
+class_name Nokia3310 ,"res://Assets/Weapons/indestructible_nokia_3310/Indestructible_Nokia_3310.png"
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
