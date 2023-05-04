cfc41a44c27e8d50f6af55702c1b0d50bb917733
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2023-05-04 14:49:02 +0700

    add new skill

---
+++ Game Project/Skills/GottaGoFast/GottaGoFast.gd
@@ -0,0 +1,13 @@
+extends "res://Skills/Skill.gd"
+
+class_name GottaGoFast,"res://Assets/Skills/gotta_go_fast.png"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
