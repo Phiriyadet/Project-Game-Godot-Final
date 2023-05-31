b974f051fac39a703b9fbb8fc75b303ca251e9eb
Mig113366 <130024571+NeroNero002@users.noreply.github.com>
2023-05-31 17:13:20 +0700

    Revert "ีอัพใหม่แก้ Error"
    This reverts commit deca0b9d027c51ae61fed533bf2fecfc7b62dd09.

--- Game Project/Weapons/Nokia3310/Nokia3310.gd
+++ Game Project/Weapons/Nokia3310/Nokia3310.gd
@@ -3,7 +3,7 @@ extends "res://Weapons/Weapon.gd"
 class_name Nokia3310 ,"res://Assets/Weapons/indestructible_nokia_3310/Indestructible_Nokia_3310.png"
 # Declare member variables here. Examples:
 # var a = 2
-# var b = "text"/
+# var b = "text"
 onready var thisNode = get_node(".")
 onready var ef = get_node("Sprite")
 var check = 0
