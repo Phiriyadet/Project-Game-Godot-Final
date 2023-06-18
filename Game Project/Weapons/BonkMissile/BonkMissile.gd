8ec0b3ca8951ce29b84e0643c5e41903d88e6a7f
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2023-06-18 15:07:43 +0700

    แก้โค้ดให้ missile ทำงานได้แล้ว
    -ใช้การสุ่มโดยไม่ต้องตรวจจับศัตรู

--- Game Project/Weapons/BonkMissile/BonkMissile.gd
+++ Game Project/Weapons/BonkMissile/BonkMissile.gd
@@ -2,10 +2,11 @@ extends "res://Weapons/Weapon.gd"
 
 class_name BonkMissile, "res://Assets/Weapons/bonk-missile/bonk_missile-1.png"
 
-
 ### Automatic References Start ###
 onready var _attack_timer: Timer = $AttackTimer
 onready var _node2d: Node2D = $Node2D
+onready var detect_enemy_area: CollisionShape2D = $DetectEnemy/CollisionShape2D
+
 ### Automatic References Stop ###
 
 var speed = 200
@@ -20,71 +21,63 @@ var check = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	cool_down_timer.connect("timeout", self, "_on_Timer_Cooldown_timeout")
-#	angle = global_position.direction_to(target)
-#	_node2d.rotation = angle.angle()
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-#	angle = global_position.direction_to(target)
-#	angle = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized()
 	_node2d.rotation = angle.angle()
-	_node2d.position += angle * speed * delta
-
-
-func _on_AttackTimer_timeout():
-	print_debug("Missile attack timeout")
-	_node2d.visible = false
+	position += angle.normalized() * speed * delta
+	
+#เรียกใช้ใน AnimationPlayer
+func AttackTimer_timeout():
 	set_process(false)
-#	animation_player.stop()
-	cool_down_timer.start()
+	detect_enemy_area.disabled = true
+	print_debug("AttackTimer_timeout")
+#	cool_down_timer.start() #ติดปัญหาเรียกแล้วไม่ทำงาน
+
+#func _on_CoolDownTimer_timeout():
+#
+#	print_debug("CoolDownTimer_timeout")
 	
-func _on_Timer_Cooldown_timeout():
-	#animation_player.play("attack")
-	print_debug("Missile cooldawn timeout")
-	_node2d.visible = true
-	_node2d.position = Vector2.ZERO
-	target = get_random_target()
-	angle = global_position.direction_to(target)
-	
+#เรียกใช้ใน AnimationPlayer
+func launch():
 	set_process(true)
-	var random_angle = rand_range(0, 360)
-	var direction = Vector2(cos(deg2rad(random_angle)), sin(deg2rad(random_angle)))
-	angle = direction.normalized()
-	_attack_timer.start()
+	position = Vector2.ZERO
+	print_debug("position start:",position,"player po:",player.position)
 	
+	detect_enemy_area.disabled = false
+	target = get_random_target()
+	angle = position.direction_to(target)
+#	_attack_timer.start()
 	
-
 func get_random_target():
-	if enemy_close.size() > 0:
-		var ran = enemy_close[randi() % enemy_close.size()].get_global_position()
-		return ran
-	else:
-		return Vector2.UP
+#	if enemy_close.size() > 0:
+#		var ran = enemy_close[randi() % enemy_close.size()].get_global_position()
+#		return ran
+#	else:
+#		return Vector2.UP
+	var range_x = rand_range(-500, 500)  # สุ่มตำแหน่ง X ในช่วง -500 ถึง 500
+	var range_y = rand_range(-500, 500)  # สุ่มตำแหน่ง Y ในช่วง -500 ถึง 500
+	return Vector2(range_x, range_y)
 
-				
 func check_level():
 	match level:
-		1,2:
+		1, 2:
 			self.atk_w += 40
-		3,4:
+		3, 4:
 			self.atk_w += 60
-		5,6:
+		5, 6:
 			self.atk_w += 80
 		7:
 			self.atk_w += 100
 
-
-func _on_DetectEneny_area_entered(area):
-	if not enemy_close.has(area):
-		if eneNum <=0:
+func _on_DetectEnemy_area_entered(area):
+	if !enemy_close.has(area):
+		if eneNum > 0:
 			enemy_close.append(area)
-		else:
-			eneNum-=1
+			eneNum -= 1
 
-func _on_DetectEneny_area_exited(area):
+func _on_DetectEnemy_area_exited(area):
 	if enemy_close.has(area):
 		enemy_close.erase(area)
-
-
-
+		eneNum += 1
