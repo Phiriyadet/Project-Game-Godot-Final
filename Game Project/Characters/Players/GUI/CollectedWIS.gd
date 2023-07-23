a2d6cc7bcc2ad76cc1756685abe060b729f7404c
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2023-07-23 17:53:15 +0700

    แก้ปัญหาการแสดงผลในหน้าต่างแสดงของทั้งหมดเมื่อกดหยุดเกม

--- Game Project/Characters/Players/GUI/CollectedWIS.gd
+++ Game Project/Characters/Players/GUI/CollectedWIS.gd
@@ -1,19 +1,19 @@
-extends Label
+extends ColorRect
 
-onready var lbl_name = $lbl_name
-onready var lbl_desription = $lbl_description
-onready var lbl_level = $lbl_level
-onready var lbl_type = $lbl_type
-onready var itemIcon = $ColorRect/ItemIcon
+onready var lbl_name = $Details/lbl_name
+onready var lbl_desription = $Details/lbl_description
+onready var lbl_level = $Details/lbl_level
+onready var lbl_type = $Details/lbl_type
+onready var itemIcon = $ItemIcon
+onready var details = $Details
 
 var item = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	lbl_name.text = UpgradeDb.UPGRADES[item]["displayname"]
 	lbl_desription.text = UpgradeDb.UPGRADES[item]["details"]
-	lbl_level.text = "level "+ str(UpgradeDb.UPGRADES[item]["level"])
+	lbl_level.text = "level "+ str(clamp(UpgradeDb.UPGRADES[item]["level"]-1, 1, 7))
 	lbl_type.text = UpgradeDb.UPGRADES[item]["type"]
 	itemIcon.texture = load(UpgradeDb.UPGRADES[item]["icon"])
 
@@ -21,3 +21,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
--- Game Project/Menus/Pause.gd
+++ Game Project/Menus/Pause.gd
@@ -1,49 +1,40 @@
 extends Control
 
-
 ### Automatic References Start ###
 onready var _settings: Settings = $Settings
 ### Automatic References Stop ###
-
 
 onready var _all_collected = $AllCollected
 onready var collectedBox = $AllCollected/ScrollContainer/CollectedBox
 onready var collectedAllItems = preload("res://Characters/Players/GUI/CollectedWIS.tscn")
 
-
 func _ready():
 	hide()
-	if _all_collected != null:
-		_all_collected.hide()
+	_all_collected.hide()
 	_settings.hide()
-	
-
 
 func close():
 	get_tree().paused = false
 	hide()
 
 func open():
-	show()
 	ShowCollected()
-
+	show()
 
 func _on_ResumeBtn_pressed():
-	close()
 	HideCollected()
-
+	close()
 
 func _on_ExitBtn_pressed():
 	get_tree().quit()
 
-
 func _on_BackToMainMenu_pressed():
 	close()
-#	get_tree().change_scene("res://Menus/MainMenu.tscn")
 	Global.player_dead = true
 
 func ShowCollected():
 	_all_collected.show()
+	clearCollectedBox()
 	var collected_list = Global.collected_allitem
 	for i in collected_list:
 		var collected_item = collectedAllItems.instance()
@@ -52,10 +43,11 @@ func ShowCollected():
 
 func HideCollected():
 	_all_collected.hide()
-	var collected_children = collectedBox.get_children()
-	for i in collected_children:
-		i.queue_free()
+	clearCollectedBox()
 
+func clearCollectedBox():
+	for child in collectedBox.get_children():
+		child.queue_free()
 
 func _on_SettingsBtn_pressed():
 	_settings.show()
