c08e129ee5ab1ca263651e829bf0040d8b01349a
Phiriyadet <54772372+Phiriyadet@users.noreply.github.com>
2023-08-17 18:12:20 +0700

    เพิ่มเสียงจี่หอย

--- Game Project/Items/SpacialItems/JiiHoy/JiiHoy.gd
+++ Game Project/Items/SpacialItems/JiiHoy/JiiHoy.gd
@@ -13,14 +13,15 @@ func _physics_process(delta):
 	
 func _on_Area2D_body_entered(body):
 	var ani_heal = heal.instance()
+	_audio_stream_player_2d.play()
 	ani_heal.position = player.global_position + Vector2(0,-50)
 	get_parent().get_node("../Player").call_deferred("add_child",ani_heal)
 	collision_shape2D.set_deferred("disabled", true)
 	body.set_hp(body.get_hp()+50)
 	print_debug("JiiHoy add hp", body.get_hp())
-	var __ = tween.interpolate_property(self, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 0.6, Tween.TRANS_SINE, Tween.EASE_IN)
+	var __ = tween.interpolate_property(self, "modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), 0.6, Tween.TRANS_SINE, Tween.EASE_OUT)
 	assert(__)
-	__ = tween.interpolate_property(self, "position", position, position + Vector2.UP * 16, 0.6, Tween.TRANS_SINE, Tween.EASE_IN)
+	__ = tween.interpolate_property(self, "position", position, position + Vector2.UP * 16, 0.6, Tween.TRANS_SINE, Tween.EASE_OUT)
 	assert(__)
 	__ = tween.start()
 	assert(__)
